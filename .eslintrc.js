ffd1f8149ca783fe7690fa394da9fedb319ce085
Davin Lin <davin135@gmail.com>
2020-04-14 01:08:35 +0800

    update settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,30 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
+  "root": true,
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
+  "extends": [
+    "plugin:vue/essential",
+    "airbnb-base",
+    "@vue/airbnb",
   ],
-  parserOptions: {
-    parser: 'babel-eslint',
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
   },
-  rules: {
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module",
+    "parser": 'babel-eslint',
+  },
+  "plugins": [
+    "vue"
+  ],
+  "rules": {
     'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'development' ? 'off' : 'error',
     'camelcase': 'off'
-  },
+  }
 };
