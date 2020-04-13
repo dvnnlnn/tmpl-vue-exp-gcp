646b037941341a49d17d808b9f57fa4ff453bf02
Davin Lin <davin135@gmail.com>
2020-04-14 02:52:50 +0800

    ignore .vscode

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'development' ? '/' : '/',
   devServer: {
     port: 3000,
-    proxy: 'http://localhost:8888',
-  },
+    proxy: 'http://localhost:8888'
+  }
 };
