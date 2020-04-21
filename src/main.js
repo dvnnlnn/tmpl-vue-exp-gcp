a6be1ef6b464539a186a6e7117a18b0dade981b8
Davin Lin <davin135@gmail.com>
2020-04-21 18:02:29 +0800

    kill inDev

--- src/main.js
+++ src/main.js
@@ -9,17 +9,12 @@ const clientPort = 3000;
 const serverPort = 8888;
 const serverUrl = `http://localhost:${serverPort}`;
 
-// global variables
-window.inDev = process.env.NODE_ENV === 'development';
-
 // before enter
 const { href } = window.location;
-if (window.inDev && href.match(serverUrl)) {
+if (href.match(serverUrl)) {
   // redirect to client port
   window.location.href = href.replace(serverPort, clientPort);
 } else {
-  // global variable
-  window.inDev = (process.env.NODE_ENV === 'development');
   // vue config
   Vue.config.productionTip = false;
   // vue init
