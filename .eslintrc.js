53ecdb96b44944ebf307d2daf6b11f1b36bbfeef
Davin Lin <davin135@gmail.com>
2020-08-03 15:52:45 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,25 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es6': true,
-    'node': true
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
   },
-  'extends': [
-    'eslint:recommended',
-    'plugin:vue/essential',
-    'prettier'
-  ],
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly'
+  extends: ['eslint:recommended', 'plugin:vue/essential', 'prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  'parserOptions': {
-    'ecmaVersion': 2018,
-    'sourceType': 'module'
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 8,
+    sourceType: 'module',
   },
-  'plugins': [
-    'vue',
-    'prettier'
-  ],
-  'rules': {
+  plugins: ['vue', 'prettier'],
+  rules: {
     'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'development' ? 'off' : 'error',
-    'camelcase': 'off',
-    'prettier/prettier': 'error'
-  }
+    camelcase: 'off',
+    'prettier/prettier': 'error',
+  },
 };
